444fbf37756fbd5a3717f79cfc7b9089296f907a
FetisovDmytro <fetisov1990ym@gmail.com>
2023-02-19 10:59:27 +0200

    DOM & actions.

---
+++ js/task-08.js
@@ -0,0 +1,13 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  email === "" || password === ""
+    ? alert("Все поля должны быть заполнены!")
+    : console.log({ email, password });
+  loginForm.reset();
+});
