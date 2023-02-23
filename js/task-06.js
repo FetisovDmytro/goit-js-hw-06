ef9d4b964e57dee8d6bc4cfaf3c5d73a75ad4ab4
FetisovDmytro <fetisov1990ym@gmail.com>
2023-02-23 12:55:37 +0200

    Допрацювання

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,27 @@
-const inputText = document.querySelector("#validation-input");
+// const inputText = document.querySelector("#validation-input");
 
-inputText.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length === 6) {
-    inputText.classList.add("valid");
-    inputText.classList.remove("invalid");
+// inputText.addEventListener("blur", (event) => {
+//   if (event.currentTarget.value.length === 6) {
+//     inputText.classList.add("valid");
+//     inputText.classList.remove("invalid");
+//   } else {
+//     inputText.classList.add("invalid");
+//     inputText.classList.remove("valid");
+//   }
+// });
+
+
+const input = document.querySelector('#validation-input');
+const requiredLength = parseInt(input.getAttribute('data-length'));
+
+input.addEventListener('blur', () => {
+  const inputLength = input.value.length;
+  
+  if (inputLength === requiredLength) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
   } else {
-    inputText.classList.add("invalid");
-    inputText.classList.remove("valid");
+    input.classList.add('invalid');
+    input.classList.remove('valid');
   }
 });
