529b90268789f07c42bbebfba11e2f4d2a93c62a
FetisovDmytro <fetisov1990ym@gmail.com>
2023-02-23 12:51:02 +0200

    Замена

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,22 @@
-const refs = {
-  textInput: document.querySelector("#name-input"),
-  textOutput: document.querySelector("#name-output"),
-};
+// const refs = {
+//   textInput: document.querySelector("#name-input"),
+//   textOutput: document.querySelector("#name-output"),
+// };
 
-const editText = () => {
-  refs.textOutput.textContent =
-    refs.textInput.value.trim() === ""
-      ? (refs.textOutput.textContent = "Anonymous")
-      : (refs.textOutput.textContent = refs.textInput.value);
-};
+// const editText = () => {
+//   refs.textOutput.textContent =
+//     refs.textInput.value.trim() === ""
+//       ? (refs.textOutput.textContent = "Anonymous")
+//       : (refs.textOutput.textContent = refs.textInput.value);
+// };
 
-refs.textInput.addEventListener("input", editText);
-
-
+// refs.textInput.addEventListener("input", editText);
 
 
+
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', () => {
+  output.textContent = input.value.trim() === '' ? 'Anonymous' : input.value;
+});
